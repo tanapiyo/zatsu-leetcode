83805ddcc39a509231b34ea95e8fbb089f4d4b77
tanapiyo <t1820018@edu.cc.uec.ac.jp>
2021-05-08 12:24:23 +0900

    9. Palindrome Number

--- 7.reverse-integer.py
+++ 7.reverse-integer.py
@@ -27,4 +27,11 @@ class Solution:
             while strx[-1] == "0":
                 strx = strx[:-1]
         return self.return_str(strx[::-1], minusflg)
+
+
+''' beautiful answer
+sign = [1,-1][x < 0]
+rst = sign * int(str(abs(x))[::-1])
+return rst if -(2**31)-1 < rst < 2**31 else 0
+'''
 # @lc code=end
---
+++ 9.palindrome-number.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=9 lang=python3
+#
+# [9] Palindrome Number
+#
+
+# @lc code=start
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if str(x) == str(x)[::-1]:
+            return True
+        else:
+            return False
+# @lc code=end
